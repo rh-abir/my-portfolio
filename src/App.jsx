8f18a1f17cfbfe4a41482148729b6a97dd656ed6
rh-abir <rhabir.co@gmail.com>
2023-07-03 20:46:46 +0600

    make about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import About from './components/About/About'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import Skills from './components/Skills/Skills'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <main className='main'>
       <Home></Home>
       <About></About>
+      <Skills></Skills>
     </main>
     </>
   )
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,20 @@
+
+import Backend from './Backend';
+import Frontend from './Frontend';
+import './Skills.css'
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+      <h2 className="secton__title">Skills</h2>
+      <span className="section__subtitle">My technical level</span>
+       <div className="skills__container container grid">
+        <Frontend></Frontend>
+
+        <Backend></Backend>
+       </div>
+    </section>
+  );
+};
+
+export default Skills;
