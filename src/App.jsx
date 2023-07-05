be77245dd9e470e8d394681474d9c6fc068c8db2
rh-abir <rhabir.co@gmail.com>
2023-07-05 12:56:57 +0600

    make qualification section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import About from './components/About/About'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
+import Qualification from './components/Qualification/Qualification'
 import Services from './components/Services/Services'
 import Skills from './components/Skills/Skills'
 
@@ -16,6 +17,7 @@ function App() {
       <About></About>
       <Skills></Skills>
       <Services></Services>
+      <Qualification></Qualification>
     </main>
     </>
   )
