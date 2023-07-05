7270e14b4bfc051b7117b75cc070103695658a00
rh-abir <rhabir.co@gmail.com>
2023-07-05 18:36:27 +0600

    make contact form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Qualification from './components/Qualification/Qualification'
@@ -20,6 +21,7 @@ function App() {
       <Services></Services>
       <Qualification></Qualification>
       <Testimonials></Testimonials>
+      <Contact></Contact>
     </main>
     </>
   )
