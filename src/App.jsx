b73d9494fcf4391d30f0df238d7cfeb5ca8f6e19
rh-abir <rhabir.co@gmail.com>
2023-10-19 16:02:09 +0600

    add particles js

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
 import "./App.css";
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import Qualification from "./components/Qualification/Qualification";
-import ScrollUp from "./components/ScrollUp/ScrollUp";
-import Services from "./components/Services/Services";
-import Skills from "./components/Skills/Skills";
-import Work from "./components/Work/Work";
+import Main from "./Layout/Main";
+import PraticleBg from "./components/Particle/PraticleBg";
 
-function App() {
+const App = () => {
+  // return <Main></Main>;
   return (
     <>
-      <Header></Header>
-      <main className="main">
-        <Home></Home>
-        <About></About>
-        <Skills></Skills>
-        <Services></Services>
-        <Qualification></Qualification>
-        <Work></Work>
-        <Contact></Contact>
-      </main>
-      <Footer></Footer>
-      <ScrollUp></ScrollUp>
+      <PraticleBg></PraticleBg>
+      <Main></Main>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,30 @@
+import About from "../components/About/About";
+import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import Home from "../components/Home/Home";
+import Qualification from "../components/Qualification/Qualification";
+import ScrollUp from "../components/ScrollUp/ScrollUp";
+import Services from "../components/Services/Services";
+import Skills from "../components/Skills/Skills";
+import Work from "../components/Work/Work";
+const Main = () => {
+  return (
+    <>
+      <Header></Header>
+      <main className="main">
+        <Home></Home>
+        <About></About>
+        <Skills></Skills>
+        <Services></Services>
+        <Qualification></Qualification>
+        <Work></Work>
+        <Contact></Contact>
+      </main>
+      <Footer></Footer>
+      <ScrollUp></ScrollUp>
+    </>
+  );
+};
+
+export default Main;
